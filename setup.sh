be56a1e584e587c74d544ceb61e0fd481f8a4a1b
Janik Eriksson <janik.eriksson@icloud.com>
2023-11-16 15:54:01 +0100

    Used wrong variable in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 
 vimPlugPath="$HOME/.local/share/nvim/site/autoload/plug.vim"
 
-if ! test -f $path; then
-  curl -fLo $path --create-dirs \
+if ! test -f $vimPlugPath; then
+    curl -fLo $vimPlugPath --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    nvim -c "PlugInstall" -c "qa"
 fi
 
 cd $HOME/dotfiles

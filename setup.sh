c5da925931246871cbf6d3108ffe8163a72b7826
Janik Eriksson <janik.eriksson@icloud.com>
2025-07-03 14:03:59 +0200

    Change config

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 vimPlugPath="$HOME/.local/share/nvim/site/autoload/plug.vim"
 
 if ! test -f $vimPlugPath; then
-    curl -fLo $vimPlugPath --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    nvim -c "PlugInstall" -c "qa"
+  curl -fLo $vimPlugPath --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  nvim -c "PlugInstall" -c "qa"
 fi
 
 cd $HOME/dotfiles
